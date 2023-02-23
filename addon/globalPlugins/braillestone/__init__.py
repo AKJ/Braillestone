da2ec938e03d600aa94b51ba3fb2d2ad8a4f346a
AKJ <andrew@johnson5.net>
2023-02-23 13:47:43 -0500

    Alpha build. The addon technically works, but will be refined and expanded before any beta or release candidates.

---
+++ addon/globalPlugins/braillestone/__init__.py
@@ -0,0 +1,63 @@
+import gui
+from globalPluginHandler import GlobalPlugin as _GlobalPlugin
+from scriptHandler import script
+import ui
+import wx
+
+from . import hsutils
+
+cardDb= hsutils.cardLookup("cards.collectible.json") # hacky placeholder
+cardDb.loadCards()
+
+class GlobalPlugin(_GlobalPlugin):
+	def __init__(self) -> None:
+		super().__init__()
+		self.dialog = None
+	@script(
+		description = "Look up Hearthstone card from clipboard",
+		category="Braillestone",
+		gesture="kb:NVDA+h"
+	)
+	def script_FindCardFromClipboard(self, gesture):
+		selection = hsutils.getSelectedText().strip()
+		if selection:
+			lookupCard(selection)
+		else:
+			ui.message("Nothing selected.")
+
+	@script(
+		description="Find Hearthstone card dialog",
+		category="Braillestone",
+		gesture="kb:NVDA+shift+h"
+	)
+	def script_FindCardDialog(self, gesture):
+		if self.dialog:
+			if self.dialog.IsActive():
+				ui.message("dialog already open.")
+				return
+			else:
+				self.dialog.Close()
+		self.dialog = wx.TextEntryDialog(
+			gui.mainFrame,
+			"Enter card name to look up.",
+			"Look up Hearthstone card",
+			style = wx.OK|wx.CANCEL
+		)
+		def callback(result):
+			if result == wx.ID_OK:
+				text = self.dialog.GetValue().strip()
+				if text:
+					lookupCard(text)
+			self.dialog = None
+		gui.runScriptModalDialog(self.dialog, callback)
+
+
+def lookupCard(card):
+	result = cardDb.cardSearch(card, "name")
+	if not result:
+		ui.browseableMessage(
+			# Translators: The specified card could not be found.
+			f"Could not find card {card}.", "Card not found", isHtml=True)
+		return None
+	result = cardDb.displayCard(result)
+	ui.browseableMessage(result, "Hearthstone Card Lookup", isHtml=True)
---
+++ addon/globalPlugins/braillestone/hsutils.py
@@ -0,0 +1,77 @@
+import json
+import os
+from typing import List, Union
+#import api
+#from textInfos import POSITION_SELECTION
+
+class cardLookup:
+	def __init__(self, file):
+		self.file = file
+
+	def loadCards(self) -> list:
+		file = os.path.dirname(__file__) + "\\" + self.file
+		with open(file, "r", encoding="utf8") as fh:
+			self.db = json.load(fh)
+
+	def cardSearch(self, identifier: Union[int, str], param: str, multiple: bool=False) -> list:
+		if multiple:
+			cardlist = [item for item in self.db if item[param] == identifier]
+		else:
+			cardlist = next((item for item in self.db if item[param] == identifier), None)
+		return cardlist
+
+
+	def processCard(self, card: dict) -> dict:
+		# Convert any lists into strings
+		card = {key: ", ".join(value) if isinstance(value, list) else value for key, value in card.items()}
+		# Clean up some strings for display
+		if card["cost"]:
+			card["cost"] = f"{card['cost']} mana"
+		if card["type"] == "WEAPON":
+			card["stats"] = f"{card['attack']} attack, {card['durability']} durability."
+		elif card["type"] == "LOCATION":
+			card["stats"] = f"{card['durability']} durability."
+		elif card["type"] == "MINION":
+			card["stats"] = f"{card['attack']} attack, {card['health']} health."
+		else:
+			card["stats"] = None
+		keyOrder = ['name', 'cost', 'stats', 'text', 'type', 'spellSchool', 'races', 'cardClass', 'rarity', 'set', 'flavor', 'mechanics', 'artist']
+		return {k: card[k] for k in keyOrder if k in card and k is not None}
+
+	def displayCard(self, cardData: dict) -> str:
+		card = self.processCard(cardData)
+		if not card:
+			return None
+		cardstring = str()
+		for k, v in card.items():
+			cardstring += f"<p>{k}: {v}</p>\n"
+		return cardstring
+
+# adapted from Quick Dictionary by Oleksandr Gryshchenko <grisov.nvaccess@mailnull.com>
+def getSelectedText() -> str:
+	"""Retrieve the selected text.
+	If the selected text is missing - extract the text from the clipboard.
+	If the clipboard is empty or contains no text data - announce a warning.
+	@return: selected text, text from the clipboard, or an empty string
+	@rtype: str
+	"""
+	obj = api.getFocusObject()
+	treeInterceptor = obj.treeInterceptor
+	if hasattr(treeInterceptor, 'TextInfo') and not treeInterceptor.passThrough:
+		obj = treeInterceptor
+	try:
+		info = obj.makeTextInfo(POSITION_SELECTION)
+	except (RuntimeError, NotImplementedError):
+		info = None
+	if not info or info.isCollapsed:
+		try:
+			text = api.getClipData()
+		except Exception:
+			text = ''
+		if not text or not isinstance(text, str):
+			# Translators: User has pressed the shortcut key for translating selected text,
+			# but no text was actually selected and clipboard is clear
+			ui.message(_("There is no selected text, the clipboard is also empty, or its content is not text!"))
+			return ''
+		return text
+	return info.text
